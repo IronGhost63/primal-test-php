31f359c9be5a6cb6d4299f4b78c7d966db410ee3
Jay <me@jirayu.in.th>
2024-03-05 15:26:03 +0700

    fix routing for php built-in server

--- public/index.php
+++ public/index.php
@@ -6,10 +6,16 @@ use Buki\Router\Router;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../config/config.php';
 
+// Return actual resource (for cli-server)
+if ( php_sapi_name() === 'cli-server' && preg_match('/\.(?:png|jpg|css|js)$/', $_SERVER["REQUEST_URI"])) {
+    return false;    // serve the requested resource as-is.
+}
+
 if ( ! defined( 'ABSPATH' ) ) {
     define( 'ABSPATH', __DIR__ . '/../' );
 }
 
+// Routing
 $router = new Router([
     'paths' => [
       'controllers' => 'src/Controllers',
